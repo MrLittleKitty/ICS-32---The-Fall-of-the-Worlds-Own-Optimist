be12dc1cc42cf15339e6e81bafa80e06d8724fec
Mr_Little_Kitty <nuclearcat1337@gmail.com>
2018-02-27 20:00:46 -0800

    Working on the UI and method skeleton

--- columns_ui.py
+++ columns_ui.py
@@ -1,7 +1,53 @@
 # Eric Wolfe 76946154 eawolfe@uci.edu
+import game_state as game
+
 
 def start_game() -> None:
+    rows = get_int()
+    cols = get_int()
+    state = game.GameState(rows, cols)
+
+    line = next_line()
+    if line == 'CONTENTS':
+        rowList = []
+        for i in range(rows):
+            row = []
+            line = next_line()
+            for index in range(cols):
+                row.append(line[i])
+            rowList.append(row)
+        state.set_board_contents(rowList)
+
+    while not state.game_over():
+        display_board(state)
+        line = next_line()
+        if line == 'Q':
+            return
+    print('GAME OVER')
+
+
+def process_command(command: str, state: game.GameState) -> None:
+    if command == '':
+        state.tick()
+    elif command == 'R':
+        state.rotate_faller()
+    elif command == '<':
+        state.move_faller(game.LEFT)
+    elif command == '>':
+        state.move_faller(game.RIGHT)
+
+
+def display_board(state: game.GameState) -> None:
     return
+
+
+def get_int() -> int:
+    line = input().strip()
+    return int(line)
+
+
+def next_line() -> str:
+    return input().strip()
 
 
 if __name__ == '__main__':
--- game_state.py
+++ game_state.py
@@ -1,9 +1,32 @@
 # Eric Wolfe 76946154 eawolfe@uci.edu
+
+LEFT = -1
+RIGHT = 1
+
+EMPTY = ' '
+S = 'S'
+T = 'Y'
+V = 'V'
+W = 'W'
+X = 'X'
+Y = 'Y'
+Z = 'Z'
+
+
 class GameState:
     def __init__(self, rows: int, columns: int) -> None:
         super().__init__()
         self.rows = rows
         self.columns = columns
+        self.boardRows = []
+        for i in range(rows):
+            row = []
+            for j in range(columns):
+                row.append(EMPTY)
+            self.boardRows.append(row)
+
+    def set_board_contents(self, contents: [[str]]) -> None:
+        return
 
     def get_rows(self) -> int:
         return self.rows
@@ -12,4 +35,16 @@ class GameState:
         return self.columns
 
     def tick(self) -> None:
+        return
+
+    def spawn_faller(self):
+        return
+
+    def rotate_faller(self):
+        return
+
+    def move_faller(self, direction: int):
+        return
+
+    def game_over(self) -> bool:
         return
